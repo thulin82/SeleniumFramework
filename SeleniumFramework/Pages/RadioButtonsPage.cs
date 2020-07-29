c245fdde590be1f3213ad292c636efdb8c6fa222
Markus Thulin <macky_b@hotmail.com>
2020-07-29 10:32:14 +0200

    Added RadioButton test cases

--- SeleniumFramework/Pages/RadioButtonsPage.cs
+++ SeleniumFramework/Pages/RadioButtonsPage.cs
@@ -8,5 +8,19 @@ namespace SeleniumFramework.Pages
         private readonly IWebDriver _driver;
 
         public RadioButtonsPage() => _driver = DriverContext.Driver;
+
+        #region WebElements
+        public IWebElement SingleMaleRadio => _driver.FindElement(By.XPath("//body/div[@id='easycont']/div[@class='row']/div[@class='col-md-6 text-left']/div[@class='panel panel-default']/div[@class='panel-body']/label[1]"));
+        public IWebElement SingleFemaleRadio => _driver.FindElement(By.XPath("//body/div[@id='easycont']/div[@class='row']/div[@class='col-md-6 text-left']/div[@class='panel panel-default']/div[@class='panel-body']/label[2]"));
+        public IWebElement GetSingleCheckedButton => _driver.FindElement(By.Id("buttoncheck"));
+        public IWebElement SingleDisplay => _driver.FindElement(By.ClassName("radiobutton"));
+        public IWebElement MultiFemaleRadio => _driver.FindElement(By.XPath("//div[@class='panel-body']//div//label[@class='radio-inline'][contains(text(),'Female')]"));
+        public IWebElement MultiMaleRadio => _driver.FindElement(By.XPath("//div[@class='panel-body']//div//label[@class='radio-inline'][contains(text(),'Male')]"));
+        public IWebElement MultiZeroToFive => _driver.FindElement(By.XPath("//label[contains(text(),'0 to 5')]"));
+        public IWebElement MultiFiveToFifteen => _driver.FindElement(By.XPath("//label[contains(text(),'5 to 15')]"));
+        public IWebElement MultiFifteenToFifty => _driver.FindElement(By.XPath("//label[contains(text(),'15 to 50')]"));
+        public IWebElement MultiGetValuesButton => _driver.FindElement(By.XPath("//button[contains(text(),'Get values')]"));
+        public IWebElement MultiDisplay => _driver.FindElement(By.ClassName("groupradiobutton"));
+        #endregion
     }
 }
--- SeleniumFramework/Tests/RadioButtonsTestCases.cs
+++ SeleniumFramework/Tests/RadioButtonsTestCases.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using SeleniumFramework.Base;
-using SeleniumFramework.Config;
 using SeleniumFramework.Pages;
 
 namespace SeleniumFramework.Tests
@@ -19,9 +18,37 @@ namespace SeleniumFramework.Tests
         }
 
         [Test]
-        public void DefaultTest()
+        public void SingeMale()
         {
-            Assert.That(DriverContext.Driver.Title == Settings.RadioButtonTitle);
+            radioButtonPage.SingleMaleRadio.Click();
+            radioButtonPage.GetSingleCheckedButton.Click();
+            Assert.That(radioButtonPage.SingleDisplay.Text == "Radio button 'Male' is checked");
+        }
+
+        [Test]
+        public void SingeFemale()
+        {
+            radioButtonPage.SingleFemaleRadio.Click();
+            radioButtonPage.GetSingleCheckedButton.Click();
+            Assert.That(radioButtonPage.SingleDisplay.Text == "Radio button 'Female' is checked");
+        }
+
+        [Test]
+        public void MultiMaleFiveToFifteen()
+        {
+            radioButtonPage.MultiMaleRadio.Click();
+            radioButtonPage.MultiFiveToFifteen.Click();
+            radioButtonPage.MultiGetValuesButton.Click();
+            Assert.That(radioButtonPage.MultiDisplay.Text == "Sex : Male\r\nAge group: 5 - 15");
+        }
+
+        [Test]
+        public void MultiFemaleZeroToFive()
+        {
+            radioButtonPage.MultiFemaleRadio.Click();
+            radioButtonPage.MultiZeroToFive.Click();
+            radioButtonPage.MultiGetValuesButton.Click();
+            Assert.That(radioButtonPage.MultiDisplay.Text == "Sex : Female\r\nAge group: 0 - 5");
         }
 
         [OneTimeTearDown]
